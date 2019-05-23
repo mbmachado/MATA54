241e120fdfa9f303fa8f68e2f9083e55f0d3c2ab
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-05-23 20:19:17 -0300

    remove flags desnecessárias de multilist.cpp

--- multilist.cpp
+++ multilist.cpp
@@ -4,97 +4,82 @@
 #include "file.h"
 
 void insert() {
-	bool keyExists = false;
-	char content[51];
-	SKRecord newRecord;
-	SKRecord oldRecord;
-	FILE *multilist;
+    char content[51];
+    SKRecord newRecord;
+    SKRecord oldRecord;
+    FILE *multilist;
 
-	scanf("%s", newRecord.key);
-	scanf("%s", content);
+    scanf("%s", newRecord.key);
+    scanf("%s", content);
 
-	multilist = fopen("multilist.txt", "r+b");
-	fseek(multilist, 0, SEEK_SET);
+    multilist = fopen("multilist.txt", "r+b");
+    fseek(multilist, 0, SEEK_SET);
 
-	while(fread(&oldRecord, sizeof(SKRecord), 1, multilist)) {
-		if(strncmp(oldRecord.key, newRecord.key, 20) == 0 && oldRecord.isDeleted == 0) {
-			keyExists = true;
-			break;
-		}
-	}
+    while(fread(&oldRecord, sizeof(SKRecord), 1, multilist)) {
+        if(strncmp(oldRecord.key, newRecord.key, 20) == 0 && oldRecord.isDeleted == 0) {
+            long position = insertRecord(newRecord.key, content);
 
-	if(keyExists) {
-		long position = insertRecord(newRecord.key, content);
+            // Atualizar apontador do ex-último registro em primary file //
+            updatePointer(oldRecord.lastRecord, position);
 
-		// Atualizar apontador do ex-último registro em primary file //
-		updatePointer(oldRecord.lastRecord, position);
+            // Atualizar valor de lastRecord do registro em multilist //
+            oldRecord.lastRecord = position;
+            fseek(multilist, ftell(multilist) - sizeof(SKRecord), SEEK_SET);
+            fwrite(&oldRecord, sizeof(SKRecord), 1, multilist);
+        } else {
+            long position = insertRecord(newRecord.key, content);
 
-		// Atualizar valor de lastRecord do registro em multilist //
-		oldRecord.lastRecord = position;
-		fseek(multilist, ftell(multilist) - sizeof(SKRecord), SEEK_SET);
-		fwrite(&oldRecord, sizeof(SKRecord), 1, multilist);
-	} else {
-		long position = insertRecord(newRecord.key, content);
+            newRecord.firstRecord = position;
+            newRecord.lastRecord = position;
+            newRecord.isDeleted = 0;
 
-		newRecord.firstRecord = position;
-	 	newRecord.lastRecord = position;
-	 	newRecord.isDeleted = 0;
+            fseek(multilist, 0, SEEK_END);
+            fwrite(&newRecord, sizeof(SKRecord), 1, multilist);
+        }
+        break;
+    }
 
-	 	fseek(multilist, 0, SEEK_END);
-		fwrite(&newRecord, sizeof(SKRecord), 1, multilist);
-	}
-
-	fclose(multilist);
+    fclose(multilist);
 }
 
 void remove() {
-	bool keyExists = false;
-	SKRecord record;
-	char key[21];
-	FILE *multilist;
+    SKRecord record;
+    char key[21];
+    FILE *multilist;
 
-	scanf("%s", key);
+    scanf("%s", key);
 
-	multilist = fopen("multilist.txt", "r+b");
-	fseek(multilist, 0, SEEK_SET);
+    multilist = fopen("multilist.txt", "r+b");
+    fseek(multilist, 0, SEEK_SET);
 
-	while(fread(&record, sizeof(SKRecord), 1, multilist)) {
-		if(strncmp(record.key, key, 20) == 0 && record.isDeleted == 0) {
-			keyExists = true;
-			break;
-		}
-	}
+    while(fread(&record, sizeof(SKRecord), 1, multilist)) {
+        if(strncmp(record.key, key, 20) == 0 && record.isDeleted == 0) {
+            record.isDeleted = 1;
+            fseek(multilist, ftell(multilist) - sizeof(SKRecord), SEEK_SET);
+            fwrite(&record, sizeof(SKRecord), 1, multilist);
+            break;
+        }
+    }
 
-	if(keyExists) {
-		record.isDeleted = 1;
-		fseek(multilist, ftell(multilist) - sizeof(SKRecord), SEEK_SET);
-		fwrite(&record, sizeof(SKRecord), 1, multilist);
-	}
-
-	fclose(multilist);
+    fclose(multilist);
 }
 
 void consult() {
-	bool keyExists = false;
-	SKRecord record;
-	char key[21];
-	FILE *multilist;
+    SKRecord record;
+    char key[21];
+    FILE *multilist;
 
-	scanf("%s", key);
+    scanf("%s", key);
 
-	multilist = fopen("multilist.txt", "rb");
-	fseek(multilist, 0, SEEK_SET);
+    multilist = fopen("multilist.txt", "rb");
+    fseek(multilist, 0, SEEK_SET);
 
-	while(fread(&record, sizeof(SKRecord), 1, multilist)) {
-		if(strncmp(record.key, key, 20) == 0 && record.isDeleted == 0) {
-			keyExists = true;
-			break;
-		}
-	}
+    while(fread(&record, sizeof(SKRecord), 1, multilist)) {
+        if(strncmp(record.key, key, 20) == 0 && record.isDeleted == 0) {
+            consultRecords(record.firstRecord);
+            break;
+        }
+    }
 
-	fclose(multilist);
-
-	if(keyExists) {
-		consultRecords(record.firstRecord);
-	}
+    fclose(multilist);
 }
